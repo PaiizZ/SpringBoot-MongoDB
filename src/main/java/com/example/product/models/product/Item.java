8c1b0ac757cb28d1ee9b90c8e2b801757cfef0ae
PaiizZ <wanchanapon.t@ku.th>
2018-10-01 16:44:03 +0700

    remove validate

--- src/main/java/com/example/product/models/product/Item.java
+++ src/main/java/com/example/product/models/product/Item.java
@@ -4,14 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
-import javax.validation.constraints.NotNull;
-
 @Getter
 @Setter
 @Accessors(chain = true)
 public class Item {
-    @NotNull private String option1;
+    private String option1;
     private String option2;
     private String option3;
-    @NotNull private Integer price;
+    private Integer price;
 }
